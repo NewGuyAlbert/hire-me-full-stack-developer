ef77282a785f04d510e4e733313b8f56385ef614
NewGuyAlbert <albe9220@stud.kea.dk>
2022-06-28 15:49:15 +0200

    add ng-zorro-antd

--- project/apps/shop/src/app/app.module.ts
+++ project/apps/shop/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
